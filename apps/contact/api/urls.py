7439ba5c6d08c01279f65e026f0e13baee1dd3b0
Fakhri <mmmdov.fxri@bk.ru>
2023-02-19 11:54:04 +0400

    project ready to deploy

--- apps/contact/api/urls.py
+++ apps/contact/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from contact.api.views import *
 
+
+
+urlpatterns = [
+    
 path('contact',ContactApiView.as_view()),
 path('consultant',ConsultantApiView.as_view())
+
+]
--- apps/contact/api/views.py
+++ apps/contact/api/views.py
@@ -1,14 +1,35 @@
 from rest_framework.generics import CreateAPIView,RetrieveAPIView,ListAPIView
 from.serialziers import *
+from rest_framework.response import Response
+from rest_framework import status
+
 
 
 class ContactApiView(CreateAPIView):
     serializer_class=ContactSerializer
     queryset=Contact.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers=self.get_success_headers(data=serializer.data)   
+        return Response(serializer.data, status=status.HTTP_201_CREATED,headers=headers)
+
     
 
 
 class ConsultantApiView(CreateAPIView):
     serializer_class=ConsultantSerializer
     queryset=Consultant.objects.all()
+
+
+    def post(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers=self.get_success_headers(data=serializer.data)   
+        return Response(serializer.data, status=status.HTTP_201_CREATED,headers=headers)
+
+    
     
